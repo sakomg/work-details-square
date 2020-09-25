cc69f5d115bf00af84e7da4e5e6bc79f25904e90
fishand4ips <alekomegunov@gmail.com>
2020-09-25 21:49:09 +0300

    add calculate result

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let text = document.getElementById('text');
+let sumText = document.getElementById('sumText');
+let equals = [];
 
 function squareTravers() {
     let quantityTraversDetails = document.getElementById('travers').value;
     let result = SQUARE_TRAVERS * quantityTraversDetails;
     if (text.innerHTML !== '') {
-        text.innerHTML += '+' + result + 'м^2';
+        text.innerHTML += '+' + result + 'м²';
+    } else {
+        text.innerHTML += result + 'м²';
     }
-    else {
-        text.innerHTML += result + 'м^2';
-    }
+    equals.push(result);
     return result;
 }
 
@@ -16,11 +18,11 @@ function squareCircle() {
     let quantityCircleDetails = document.getElementById('circle').value;
     let result = SQUARE_CIRCLE * quantityCircleDetails;
     if (text.innerHTML !== '') {
-        text.innerHTML += '+' + result + 'м^2';
+        text.innerHTML += '+' + result + 'м²';
+    } else {
+        text.innerHTML += result + 'м²';
     }
-    else {
-        text.innerHTML += result + 'м^2';
-    }
+    equals.push(result);
     return result;
 }
 
@@ -28,14 +30,24 @@ function squareBigCircle() {
     let quantityBigCircleDetails = document.getElementById('big-circle').value;
     let result = BIG_SQUARE_CIRCLE * quantityBigCircleDetails;
     if (text.innerHTML !== '') {
-        text.innerHTML += '+' + result + 'м^2';
+        text.innerHTML += '+' + result + 'м²';
+    } else {
+        text.innerHTML += result + 'м²';
     }
-    else {
-        text.innerHTML += result + 'м^2';
-    }
+    equals.push(result);
     return result;
 }
 
-function sum() {
+function sumSquareDetails() {
+    let sum = 0;
+    for (let i = 0; i < equals.length; i++) {
+        sum += equals[i]
+    }
+    sumText.innerHTML += ' = ' + sum + 'м²';
+    sumText.style.color = 'red'
+}
 
+function clearSquareDetails() {
+    text.innerHTML = '';
+    sumText.innerHTML = '';
 }
